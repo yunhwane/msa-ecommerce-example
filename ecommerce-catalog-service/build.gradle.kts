04163f902ccac66237d54bb509c0a71d90d1f94c
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2025-03-01 11:01:59 +0900

    fix: add catalog dependency

--- ecommerce-catalog-service/build.gradle.kts
+++ ecommerce-catalog-service/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
